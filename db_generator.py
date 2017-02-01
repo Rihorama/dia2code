018966c609553b3f524f69f49aac54d2dac3a8cb
Alice Pavlikova <inner.dreamer@gmail.com>
2017-02-01 20:46:54 +0100

    Added a special sorting method to db_generator which sorts tables from table_dict to a table_list. The order in table_list is such that no table references another table that hasn't yet been declared.

--- db_generator.py
+++ db_generator.py
@@ -28,6 +28,9 @@ class DatabaseGenerator:
             table_dict (Dictionary): Dictionary of Table instances.
         """
         
+        #gets properly sorted list of tables
+        table_list = self.sortTables(table_dict)
+        
         #IN FILE
         if self.saving_type == "in_file":
             
@@ -39,9 +42,8 @@ class DatabaseGenerator:
             
             
             #Cycles over tables and adds them to the file
-            for i in sorted(table_dict):
+            for table in table_list:
                 
-                table = table_dict[i]
                 self.txt.startTable(table)
                 
                 #adding individual rows
@@ -59,9 +61,8 @@ class DatabaseGenerator:
         #ON TERMINAL
         else:
             #Cycles over tables and adds them to the file
-            for i in sorted(table_dict):
+            for table in table_list:
                 
-                table = table_dict[i]
                 self.txt.startTable(table)
                 
                 #adding individual rows
@@ -74,4 +75,66 @@ class DatabaseGenerator:
                 print(s)
                 
         return
+    
+    
+    
+    def sortTables(self,table_dict):
+        """Cycles over the table dict (over its hard copy) and in each cycle
+        moves specific tables to a newly created list until the dictionary
+        is empty. The moved tables are always those that reference
+        tables alrady present in the new list (or have no foreign keys).
+        This is done to sort the tables in the order that will
+        be then valid in the database dump since table x can't reference
+        another table y that hasn|t been declared yet.
+        
+        Args:
+            dict (Dictionary): Dictionary of Table instances.
+            
+        Returns:
+            table_list (List): List of Table instances, desirably sorted.
+        """
+        
+        table_list = []
+        table_dict = dict(table_dict)
+        table_dict_temp = dict(table_dict)
+        max_iter = 100
+        
+        
+        for i in range(max_iter):
+            
+            for t_id in table_dict:
+                
+                table = table_dict[t_id]
+                
+                #not referencing tables can be moved right away
+                if len(table.f_key_attr_list) == 0:                    
+                    table_list.append(table)
+                    del table_dict_temp[t_id]
+                
+                #referencing tables must be checked if their references
+                #already present in table_list. If not => ignore for now
+                else:
+                    flag = True
+                    
+                    for fk in table.f_key_attr_list:
+                        referenced_table = fk[0].my_table
+                        
+                        if referenced_table not in table_list:
+                            flag = False
+                            break
+                    
+                    #all refs in table_list, we can add this table too
+                    if flag == True:
+                        table_list.append(table)
+                        del table_dict_temp[t_id]
+                        
+            #updating the table_dict
+            table_dict = dict(table_dict_temp)
+            
+            #if the distionary has been emptied => all sorted in list, we finish
+            if len(table_dict) == 0:
+                break
+            
+        return table_list
+                    
             
